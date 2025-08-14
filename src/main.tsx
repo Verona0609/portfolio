e06724e1b9196662c58c0e9cb20ad91c96a4be91
Verona0609 <156179525+Verona0609@users.noreply.github.com>
2025-08-14 11:46:32 +0200

    added project settings

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
+
+//import { Provider } from 'react-redux';
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+
+      <App />
+
+  </BrowserRouter>
+);
