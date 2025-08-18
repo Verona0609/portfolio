855c5b0dc2951289da1adebe0dc0f2aa88fe1562
Verona0609 <156179525+Verona0609@users.noreply.github.com>
2025-08-18 14:42:08 +0200

    added homepage

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import '../src/main.css';
 
 
 //import { Provider } from 'react-redux';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-
       <App />
-
   </BrowserRouter>
 );
