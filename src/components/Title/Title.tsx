2f74a58d4364b70a0c4d4271554588c7c8577242
Verona0609 <156179525+Verona0609@users.noreply.github.com>
2025-10-02 10:55:17 +0200

    added mobile version for  homepage

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,43 +1,39 @@
-
-import css from './Title.module.css'
+import css from './Title.module.css';
 import { Link } from 'react-router-dom';
 
 const Title = () => {
   return (
-   <div className={css.background}>
-
-  <div className={css.topCards}>
-    <Link className={`${css.card} ${css.card1}`} to="/about">
-      About Me
-    </Link>
-    <Link className={`${css.card} ${css.card1}`} to="/projects">
-      Projects
-    </Link>
-  </div>
-<div className={css.titleContainer}>
-  <h1 className={css.title}>
-    <span className={css.top}>V</span>
-    <span className={css.bottom}>E</span>
-    <span className={css.top}>R</span>
-    <span className={css.bottom}>O</span>
-    <span className={css.top}>N</span>
-    <span className={css.bottom}>I</span>
-    <span className={css.top}>K</span>
-    <span className={css.bottom}>A</span>
-  </h1>
-</div>
-  <div className={css.bottomCards}>
-    <Link className={css.card} to="/tools">
-      Tools
-    </Link>
-    <Link className={css.card} to="/justme">
-      Just Me
-    </Link>
-  </div>
-
-</div>
-
-  )
-}
+    <div className={css.background}>
+      <div className={css.topCards}>
+        <Link className={`${css.card} ${css.card1}`} to="/about">
+          About Me
+        </Link>
+        <Link className={`${css.card} ${css.card1}`} to="/projects">
+          Projects
+        </Link>
+      </div>
+      <div className={css.titleContainer}>
+        <h1 className={css.title}>
+          <span className={css.top}>V</span>
+          <span className={css.bottom}>E</span>
+          <span className={css.top}>R</span>
+          <span className={css.bottom}>O</span>
+          <span className={css.top}>N</span>
+          <span className={css.bottom}>I</span>
+          <span className={css.top}>K</span>
+          <span className={css.bottom}>A</span>
+        </h1>
+      </div>
+      <div className={css.bottomCards}>
+        <Link className={css.card} to="/tools">
+          Tools
+        </Link>
+        <Link className={css.card} to="/justme">
+          Just Me
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default Title;
