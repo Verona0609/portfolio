8f99349feeece1f37afebf583054cefbaea42525
Verona0609 <156179525+Verona0609@users.noreply.github.com>
2025-11-07 16:53:20 +0100

    refactor images to /piblic

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import '../src/main.css';
-
+import { StrictMode } from 'react';
 
 //import { Provider } from 'react-redux';
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <StrictMode>
+    <BrowserRouter>
       <App />
-  </BrowserRouter>
+    </BrowserRouter>
+  </StrictMode>
 );
